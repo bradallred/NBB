47733c948dfab693035956c154a32477f201b473
Brad Allred <bradallred@me.com>
2012-10-18 16:55:10 -0600

    Initial creation of module classes

---
+++ Neon Boom Box/VideoModule/NBBVideoModule.h
@@ -0,0 +1,14 @@
+//
+//  NBBVideoModule.h
+//  Neon Boom Box
+//
+//  Created by Brad on 10/18/12.
+//  Copyright (c) 2012 NBB. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "NBBModule.h"
+
+@interface NBBVideoModule : NBBModule
+
+@end
