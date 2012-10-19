241b291d89349c3b719e0f93e12447d2b34e29af
Brad Allred <bradallred@me.com>
2012-10-19 10:10:15 -0600

    Add initial class files for interface classes
    All are subclasses of standard AppKit classes

---
+++ Neon Boom Box/NBBCore/NBBWindow.m
@@ -0,0 +1,13 @@
+//
+//  NBBWindow.m
+//  Neon Boom Box
+//
+//  Created by Brad on 10/19/12.
+//  Copyright (c) 2012 NBB. All rights reserved.
+//
+
+#import "NBBWindow.h"
+
+@implementation NBBWindow
+
+@end
