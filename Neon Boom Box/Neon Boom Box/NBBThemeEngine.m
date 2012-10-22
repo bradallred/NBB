febcf639f8d2e9c888788d1be0ef86bbe0b10806
Brad Allred <bradallred@me.com>
2012-10-22 10:04:50 -0600

    ThemeEngine: initialization and deallocation method implementation

--- Neon Boom Box/Neon Boom Box/NBBThemeEngine.m
+++ Neon Boom Box/Neon Boom Box/NBBThemeEngine.m
@@ -55,4 +55,35 @@ static NBBThemeEngine* _sharedThemeEngine = nil;
 
 // end singleton pattern
 
+- (id)init
+{
+    self = [super init];
+    if (self) {
+		_themedObjects = [[NSMutableArray alloc] init];
+        // get the current theme from user defaults
+		// if the theme is not set or no longer exists
+		// we will apply the first theme found
+		NSString* themeName = [[NSUserDefaults standardUserDefaults] stringForKey:@"NBBActiveTheme"];
+		NSString* themePath = [[NSBundle mainBundle] pathForResource:themeName ofType:@"nbbtheme" inDirectory:@"Themes"];
+		if (!themePath) {
+			// just grab the first nbbtheme bundle
+			NSString* themeDir = [NSString stringWithFormat:@"%@/Themes", [[NSBundle mainBundle] bundlePath]];
+			NSArray* themes = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:themeDir error:nil];
+			themes = [themes filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"self ENDSWITH '.nbbtheme'"]];
+			themePath = [themes objectAtIndex:0];
+		}
+		NSBundle* themeBundle = [NSBundle bundleWithPath:themePath];
+		NBBTheme* theme = [[NBBTheme alloc] initWithBundle:themeBundle];
+		[self applyTheme:theme];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [_theme release];
+	[_themedObjects release];
+    [super dealloc];
+}
+
 @end
