f4cde62421ad6c627782718ce276d2885bf587a8
Brad Allred <bradallred@me.com>
2012-10-22 09:48:49 -0600

    ThemeEngine: apply a singleton pattern

--- Neon Boom Box/Neon Boom Box/NBBThemeEngine.m
+++ Neon Boom Box/Neon Boom Box/NBBThemeEngine.m
@@ -10,4 +10,49 @@
 
 @implementation NBBThemeEngine
 
+// singleton pattern
+#pragma mark - Singleton
+
+static NBBThemeEngine* _sharedThemeEngine = nil;
+
++ (NBBThemeEngine*)sharedThemeEngine
+{
+    if (_sharedThemeEngine == nil) {
+        _sharedThemeEngine = [[super allocWithZone:NULL] init];
+    }
+    return _sharedThemeEngine;
+}
+
++ (id)allocWithZone:(NSZone *)zone
+{
+    return [[self sharedThemeEngine] retain];
+}
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    return self;
+}
+
+- (id)retain
+{
+    return self;
+}
+
+- (NSUInteger)retainCount
+{
+    return NSUIntegerMax;  //denotes an object that cannot be released
+}
+
+- (oneway void)release
+{
+    //do nothing
+}
+
+- (id)autorelease
+{
+    return self;
+}
+
+// end singleton pattern
+
 @end
