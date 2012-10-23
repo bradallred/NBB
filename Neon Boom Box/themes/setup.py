f12d722c195c0020339fbf8fcba73bde5906eef6
Brad Allred <bradallred@me.com>
2012-10-23 11:06:57 -0600

    theme setup.py: tweaking the build options for the theme

--- Neon Boom Box/themes/setup.py
+++ Neon Boom Box/themes/setup.py
@@ -1,5 +1,14 @@
 from distutils.core import setup
 import py2app
 setup(
-    plugin = ['themes/nbb/nbb_theme.py']
+    plugin = ['themes/nbb/nbb_theme.py'],
+	data_files = ['themes/NBBTheme.py'],
+	options=dict(py2app=dict(
+							 extension='.nbbtheme',
+							 semi_standalone = True,
+							 plist=dict(
+										NSPrincipalClass="NBBTheme",
+										)
+						   )
+				 )
 )
