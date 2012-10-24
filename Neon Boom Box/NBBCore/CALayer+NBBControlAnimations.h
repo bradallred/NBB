bf043bd656aa59527efa5f27027fba6ce00b5792
Brad Allred <bradallred@me.com>
2012-10-23 19:55:06 -0600

    Add wiggle animation code to be used by our swappable controls

---
+++ Neon Boom Box/NBBCore/CALayer+NBBControlAnimations.h
@@ -0,0 +1,13 @@
+//
+//  CALayer+NBBControlAnimations.h
+//  Neon Boom Box
+//
+//  Created by Brad on 10/23/12.
+//  Copyright (c) 2012 NBB. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface CALayer (NBBControlAnimations)
+
+@end
---
+++ Neon Boom Box/NBBCore/CALayer+NBBControlAnimations.m
@@ -0,0 +1,13 @@
+//
+//  CALayer+NBBControlAnimations.m
+//  Neon Boom Box
+//
+//  Created by Brad on 10/23/12.
+//  Copyright (c) 2012 NBB. All rights reserved.
+//
+
+#import "CALayer+NBBControlAnimations.h"
+
+@implementation CALayer (NBBControlAnimations)
+
+@end
