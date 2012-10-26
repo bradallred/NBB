515183fb9192cd0076712278b09a6d3272497ca2
Brad Allred <bradallred@me.com>
2012-10-26 09:45:53 -0600

    add NSActionCell category to handle control swapping

---
+++ Neon Boom Box/NBBCore/NSActionCell+NBBSwappable.h
@@ -0,0 +1,27 @@
+//
+//  NSActionCell+NBBSwappable.h
+//  Neon Boom Box
+//
+//  Created by Brad on 10/25/12.
+//  Copyright (c) 2012 NBB. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "NBBSwappableControl.h"
+
+/*
+ This category will implement most of the NBBSwappableControl protocol
+ since the same code will apply to ALL	action cells
+ 
+ We will use iOS style icon "jiggling".
+ Touch and hold a swappable control to cause it and all COMPATIBLE swappable controls to jiggle
+ While jiggling you may drag a control onto another jiggling control to initiate a swap
+ */
+
+@interface NSActionCell (NBBSwappable) <NBBSwappableControl>
+@property (nonatomic, retain) id <NBBSwappableControlDelegate> swapDelegate;
+
+- (void)setSwappingEnabled:(BOOL) enable;
+- (BOOL)swappingEnabled;
+- (void)swapStateChanged:(NSNotification*) notification;
+@end
