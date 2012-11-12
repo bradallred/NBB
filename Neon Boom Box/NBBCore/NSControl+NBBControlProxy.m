85bed31720308f21e60eab95294fff88e2256d81
Brad Allred <bradallred@me.com>
2012-11-11 19:57:41 -0700

    we only care about themeing controls when they have a window

--- Neon Boom Box/NBBCore/NSControl+NBBControlProxy.m
+++ Neon Boom Box/NBBCore/NSControl+NBBControlProxy.m
@@ -31,7 +31,7 @@
 // use this method over awakeFromNib because we need to catch controls that are created dynamically too
 - (void)viewWillMoveToWindow:(NSWindow *)newWindow
 {
-	if ([self conformsToProtocol:@protocol(NBBThemable)]) {
+	if (newWindow && [self conformsToProtocol:@protocol(NBBThemable)]) {
 		[self setWantsLayer:YES]; // nbb uses CA for many control animations
 		NBBThemeEngine* themeEngine = [NBBThemeEngine sharedThemeEngine];
 		[themeEngine themeObject:(id <NBBThemable>)self];
