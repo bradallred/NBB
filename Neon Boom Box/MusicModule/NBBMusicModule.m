736584bdb7c6a8b4b20affa2e6a4b6fe5117512e
Brad Allred <bradallred@me.com>
2012-11-17 11:46:11 -0700

    MusicModule: open iTunes library for parsing

--- Neon Boom Box/MusicModule/NBBMusicModule.m
+++ Neon Boom Box/MusicModule/NBBMusicModule.m
@@ -12,4 +12,34 @@
 
 @implementation NBBMusicModule
 
+- (id)initWithWindowNibName:(NSString *)windowNibName
+{
+	self = [super initWithWindowNibName:windowNibName];
+    if (self) {
+        NSLog(@"initialize music module...");
+
+		NSArray* dbs = [[[NSUserDefaults standardUserDefaults] persistentDomainForName:@"com.apple.iApps"] objectForKey:@"iTunesRecentDatabases"];
+		NSURL* libraryURL = (([dbs count])) ? [NSURL URLWithString:[dbs objectAtIndex:0]] : nil;
+		NSLog(@"using iTunes XML location:%@", libraryURL);
+
+		// TODO: only reparse the library if it has changed.
+		// otherwise we should load our own archived representation
+
+		NSDictionary* library = [NSDictionary dictionaryWithContentsOfURL:libraryURL];
+		if (library) {
+			NSDictionary* tracks = [library objectForKey:@"Tracks"];
+			NSDictionary* playlists = [library objectForKey:@"Playlists"];
+
+			BOOL isBook = NO;
+			BOOL isVideo = NO;
+
+			for (NSDictionary* track in tracks.allValues) {
+				// TODO: import apropriate tracks into an internal library
+			}
+		}
+    }
+
+    return self;
+}
+
 @end
